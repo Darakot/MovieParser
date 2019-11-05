66949cfb0f4bfabc554fd8f3f379a2bf74c36add
Darakot <dmitriy0305@yandex.ru>
2019-11-05 10:22:12 +0500

    add class Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,27 @@
+package main.java;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        Document page = null;
+        try {
+            page = Jsoup.connect("https://weburg.net/movies/info/36962")
+                    .userAgent("Chrome/74.0.3729.169 Safari/537.36")
+                    .referrer("http://www.google.com")
+                    .get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        page.outputSettings().charset("UTF-8");
+//        page.outputSettings().charset("windows-1251");
+
+        Elements element = page.select("a.wb-link");
+        System.out.println(element);
     }
 }
