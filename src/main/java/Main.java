438949eb0bb9938dbdb8f1a42aa037b56e10f2e9
Darakot <dmitriy0305@yandex.ru>
2019-11-15 16:23:59 +0500

    Wrote a parser for 1 request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,60 @@
 package main.java;
 
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String url = "https://weburg.net/movies/info/1";
         Document page = null;
         try {
-            page = Jsoup.connect("https://weburg.net/movies/info/36962")
+            page = Jsoup.connect(url)
                     .userAgent("Chrome/74.0.3729.169 Safari/537.36")
                     .referrer("http://www.google.com")
                     .get();
-        } catch (IOException e) {
+        }catch (NullPointerException e) {
+            e.getMessage();}
+        catch (HttpStatusException e) {
+            System.out.println(e.getStatusCode());
+        }catch (IOException e) {
             e.printStackTrace();
         }
-//        page.outputSettings().charset("UTF-8");
-//        page.outputSettings().charset("windows-1251");
 
-        Elements element = page.select("a.wb-link");
-        System.out.println(element);
+//        Map<String,String> film = new HashMap<>();
+//        Element nameFilm = page.select("h3.wb-promo-box-heading").first();
+//        Element annotationFilm = page.select("table.wb-layout-about").first();
+//        Element directorFilm = page.select("li.wb-tags-series-item").first();
+//        Elements elements = page.select("div.wb-tags-series");
+//        String sDirectorFilm = new String(directorFilm.text().getBytes("UTF-8"));
+//        String sAnnotationFilm = new String(annotationFilm.select("p").text().getBytes("UTF-8"));
+//        String sNameFilm = new String(nameFilm.text().getBytes("UTF-8"));
+//        film.put("Название фильма",sNameFilm);
+//        film.put("Режиссер",sDirectorFilm);
+//        film.put("Описание",sAnnotationFilm);
+
+
+        if (page!=null) {
+            Elements elements = page.select("div.wb-tags-series");
+//        for (String s :film.keySet()) {
+//            System.out.println(s + ":" + film.get(s));
+//        }
+
+
+            for (Element e:elements) {
+                String s = new String(e.text().getBytes("UTF-8"));
+                System.out.println(s);
+            }
+        }
+
+
     }
 }
